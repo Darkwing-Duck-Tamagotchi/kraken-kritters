8f2c4b06303863d1ef6399edcd0de9b43e17a757
awint00r <austinwinterberg@yahoo.com>
2024-10-17 18:41:22 -0500

    Added array of available character objects and used Math.random to select one of the characters.

--- js/app.js
+++ js/app.js
@@ -1,1 +1,58 @@
 // Main JS Application File
+
+let availableCharacters = [{
+    species: 'string',
+    imageUrl: image1.jpg,
+    stats: {
+        hunger: 100,
+        happiness: 100,
+        cleanliness: 100,
+        energy: 100,
+    }
+},
+{
+    species: 'string',
+    imageUrl: image2.jpg,
+    stats: {
+        hunger: 100,
+        happiness: 100,
+        cleanliness: 100,
+        energy: 100,
+    }
+},
+{
+    species: 'string',
+    imageUrl: image3.jpg,
+    stats: {
+        hunger: 100,
+        happiness: 100,
+        cleanliness: 100,
+        energy: 100,
+    }
+},
+{
+    species: 'string',
+    imageUrl: image4.jpg,
+    stats: {
+        hunger: 100,
+        happiness: 100,
+        cleanliness: 100,
+        energy: 100,
+    }
+},
+{
+    species: 'string',
+    imageUrl: image5.jpg,
+    stats: {
+        hunger: 100,
+        happiness: 100,
+        cleanliness: 100,
+        energy: 100,
+    }
+}
+]
+function randomCharacter() {
+    let randomCharacters = Math.floor(Math.random() * availableCharacters.length - 1)
+    pet = availableCharacters[randomCharacters];
+}
+
