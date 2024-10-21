832c3424728999f64a4aaa93a60b47617c1d2f43
awint00r <austinwinterberg@yahoo.com>
2024-10-20 19:58:42 -0500

    New Changes to Javascript

--- js/app.js
+++ js/app.js
@@ -2,138 +2,109 @@
 
 
 let availableCharacters = [{
-    species: 'string',
+    species: 'a',
     // imageUrl: image1.jpg,
-    stats: {
-        hunger: 100,
-        happiness: 100,
-        cleanliness: 100,
-        energy: 100,
-    }
 },
 {
-    species: 'string',
+    species: 'b',
     // imageUrl: image2.jpg,
-    stats: {
-        hunger: 100,
-        happiness: 100,
-        cleanliness: 100,
-        energy: 100,
-    }
 },
 {
-    species: 'string',
+    species: 'c',
     // imageUrl: image3.jpg,
-    stats: {
-        hunger: 100,
-        happiness: 100,
-        cleanliness: 100,
-        energy: 100,
-    }
 },
 {
-    species: 'string',
+    species: 'd',
     // imageUrl: image4.jpg,
-    stats: {
-        hunger: 100,
-        happiness: 100,
-        cleanliness: 100,
-        energy: 100,
-    }
 },
 {
-    species: 'string',
+    species: 'e',
     // imageUrl: image5.jpg,
-    stats: {
-        hunger: 100,
-        happiness: 100,
-        cleanliness: 100,
-        energy: 100,
-    }
 }
 ]
-console.log(availableCharacters)
+// console.log(availableCharacters)
 function randomCharacter() {
-    let randomNum = Math.floor(Math.random() * availableCharacters.length - 1)
-    pet = availableCharacters[randomNum];
+    let randomNum = Math.floor(Math.random() * availableCharacters.length)
+    console.log("Result of Random Number", randomNum)
+    return availableCharacters[randomNum];
 }
 
 
-function endGame() {
+function endGame(pet) {
     gameIsActive = false;
+    delete pet.stats;
+    console.log("Game over", pet)
 }
 
-let pet = {
-    species: 'string',
-    // imageUrl: image.jpg,
-    stats: {
-        hunger: 100,
-        happiness: 100,
-        cleanliness: 100,
-        energy: 100,
-    }
-}
-
-function checkStats() {
+function checkStats(pet) {
     if ((pet.stats.hunger <= 0) || (pet.stats.cleanliness <=0) || (pet.stats.energy <= 0) || (pet.stats.happiness <=0) && gameIsActive) {
-        endGame();
+        endGame(pet);
     }
 }
-function decreaseStats () {
-   while (gameIsActive) {
-    pet.stats.hunger -= Math.floor(Math.random() * 5) + 1;
-    pet.stats.cleanliness -= Math.floor(Math.random() * 5) + 1; 
-    pet.stats.energy -= Math.floor(Math.random() * 5) + 1;
-    pet.stats.happiness -= Math.floor(Math.random() * 5) + 1;
-    checkStats();
-   }
+function decreaseStats(pet) {
+    console.log("Decreasing Stats", pet)
+    setInterval(function() {
+        if (gameIsActive) {
+            pet.stats.hunger -= Math.floor(Math.random() * 5) + 1;
+            pet.stats.cleanliness -= Math.floor(Math.random() * 5) + 1; 
+            pet.stats.energy -= Math.floor(Math.random() * 5) + 1;
+            pet.stats.happiness -= Math.floor(Math.random() * 5) + 1;
+            console.log("Stats Decreased", pet)
+            checkStats(pet);
+        }
+    }, 500);
 }
-
-setInterval(decreaseStats, 10000);
-
 
 let gameIsActive = false;
 function startGame() {
     gameIsActive = true;
-    console.log(pet);
+    let pet = randomCharacter();
+    pet.stats = {
+        hunger: 100,
+        happiness: 100,
+        cleanliness: 100,
+        energy: 100,
+    }
+    console.log("Game start", pet)
+    decreaseStats(pet);
 }
 
 document.getElementById("start-game").addEventListener("click", startGame)
 
 
 
-function increaseStats(statType) {
-    switch(statType) {
-        case 'hunger':
-            if (pet.stats.hunger < 100) {
-                pet.stats.hunger += Math.floor(Math.random() * 5) + 1;
-            }
-            break;
-        case 'happiness':
-            if (pet.stats.happiness < 100) {
-                pet.stats.happiness += Math.floor(Math.random() * 5) + 1;
-            }
-            break;
-        case 'cleanliness':
-            if (pet.stats.cleanliness < 100) {
-                pet.stats.cleanliness += Math.floor(Math.random() * 5) + 1;
-            }
-            break;
-        case 'energy':
-            if (pet.stats.energy < 100) {
-                pet.stats.energy += Math.floor(Math.random() * 5) + 1;
-            }
-            break;
-        default:
-            console.log("Nothing to increase");
-            return;
-    }
-}
+// function increaseStats(statType, pet) {
+//     switch(statType) {
+//         case 'hunger':
+//             if (pet.stats.hunger < 100) {
+//                 pet.stats.hunger += Math.floor(Math.random() * 5) + 1;
+//             }
+//             break;
+//         case 'happiness':
+//             if (pet.stats.happiness < 100) {
+//                 pet.stats.happiness += Math.floor(Math.random() * 5) + 1;
+//             }
+//             break;
+//         case 'cleanliness':
+//             if (pet.stats.cleanliness < 100) {
+//                 pet.stats.cleanliness += Math.floor(Math.random() * 5) + 1;
+//             }
+//             break;
+//         case 'energy':
+//             if (pet.stats.energy < 100) {
+//                 pet.stats.energy += Math.floor(Math.random() * 5) + 1;
+//             }
+//             break;
+//         default:
+//             console.log("Nothing to increase");
+//             return;
+//     }
+// }
 
-document.getElementById('feed-button').addEventListener('click', increaseStats('hunger'));
-;
-document.getElementById('pet-button').addEventListener('click', increaseStats('happiness'));
-;
-document.getElementById('clean-button').addEventListener('click', increaseStats('cleanliness'));
-;
-document.getElementById('play-button').addEventListener('click', increaseStats('energy'));
+// document.getElementById('feed-button').addEventListener('click', increaseStats('hunger'));
+// ;
+// document.getElementById('pet-button').addEventListener('click', increaseStats('happiness'));
+// ;
+// document.getElementById('clean-button').addEventListener('click', increaseStats('cleanliness'));
+// ;
+// document.getElementById('play-button').addEventListener('click', increaseStats('energy'));
